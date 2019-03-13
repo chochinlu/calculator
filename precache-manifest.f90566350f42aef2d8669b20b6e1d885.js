afad880271d68baa1ecdaf329b5f109e3539a26d
Cho-Ching Lu <chochinlu@gmail.com>
2019-03-13 15:14:34 +0800

    Updates

---
+++ precache-manifest.f90566350f42aef2d8669b20b6e1d885.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  },
+  {
+    "revision": "17325a31f36400ad7dad",
+    "url": "/calculator/static/js/main.e1ab69d8.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/calculator/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "79542e891805b8faaa1196a6a5245a65",
+    "url": "/calculator/index.html"
+  }
+];
