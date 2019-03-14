175a5c88696ebd4610e5ab11fbca09aa1a83cb1f
Cho-Ching Lu <chochinlu@gmail.com>
2019-03-14 10:50:16 +0800

    Add operations

--- src/App.js
+++ src/App.js
@@ -27,24 +27,74 @@ const TotalEle = props => (
   </Ele>
 );
 
+const ops = {
+  add: 'ADD',
+  minus: 'MINUS',
+  multi: 'MULTIPLY',
+  divide: 'DIVIDE'
+};
+
 const App = () => {
   const [numA, setNumA] = useState(0);
   const [isFloat, setIsFloat] = useState(false);
+  const [operation, setOperation] = useState(null); //operation: add, minus, multiply, divide
+  const [numB, setNumB] = useState(null);
+
+  const handleEqual = () => {
+    if (!operation || !numB) return;
+
+    if (operation === ops.add) {
+      setNumA(numA + numB);
+    }
+
+    if (operation === ops.minus) {
+      setNumA(numA - numB);
+    }
+
+    if (operation === ops.multi) {
+      setNumA(numA * numB);
+    }
+
+    if (operation === ops.divide) {
+      setNumA(numA / numB);
+    }
+
+    setNumB(null);
+    setOperation(null);
+  };
+
+  const handleOperation = opeType => {
+    if (numA === 0 && !numB) {
+      return;
+    }
+
+    setOperation(opeType);
+  };
+
+  const setNum = () => {
+    console.log({ numA, numB });
+    return numA === 0 && !numB
+      ? { target: numA, handle: setNumA }
+      : { target: numB, handle: setNumB };
+  };
 
   const clickNum = num => {
-    if (numA === 0) {
-      setNumA(num);
+    const { target, handle } = setNum();
+    console.log({ target, handle });
+
+    if (target === 0 || !target) {
+      handle(num);
     } else {
       if (isFloat) {
-        const hasDot = numA
+        const hasDot = target
           .toString()
           .split('')
           .includes('.');
         hasDot
-          ? setNumA(parseFloat(`${numA}${num}`))
-          : setNumA(parseFloat(`${numA}.${num}`));
+          ? handle(parseFloat(`${target}${num}`))
+          : handle(parseFloat(`${target}.${num}`));
       } else {
-        setNumA(`${numA * 10 + num}`);
+        handle(`${target * 10 + num}`);
       }
     }
   };
@@ -73,13 +123,13 @@ const App = () => {
   return (
     <div className="flex flex-column vh-100">
       <Row>
-        <TotalEle>{numA}</TotalEle>
+        <TotalEle>{numB ? numB : numA}</TotalEle>
       </Row>
       <Row>
         <CalEle onClick={clearNum}>AC</CalEle>
         <CalEle onClick={switchNegativePositive}>+/-</CalEle>
         <CalEle onClick={makeNumPercent}>%</CalEle>
-        <CalEle onClick={makeNumPercent}>÷</CalEle>
+        <CalEle onClick={() => handleOperation(ops.divide)}>÷</CalEle>
       </Row>
       <Row>
         {[7, 8, 9].map(ele => (
@@ -87,7 +137,7 @@ const App = () => {
             {ele}
           </Ele>
         ))}
-        <CalEle>X</CalEle>
+        <CalEle onClick={() => handleOperation(ops.multi)}>X</CalEle>
       </Row>
       <Row>
         {[4, 5, 6].map(ele => (
@@ -95,7 +145,7 @@ const App = () => {
             {ele}
           </Ele>
         ))}
-        <CalEle>-</CalEle>
+        <CalEle onClick={() => handleOperation(ops.minus)}>-</CalEle>
       </Row>
       <Row>
         {[1, 2, 3].map(ele => (
@@ -103,14 +153,14 @@ const App = () => {
             {ele}
           </Ele>
         ))}
-        <CalEle>+</CalEle>
+        <CalEle onClick={() => handleOperation(ops.add)}>+</CalEle>
       </Row>
       <Row>
         <Ele width={50} onClick={() => clickNum(0)}>
           0
         </Ele>
         <Ele onClick={() => setFloat()}>.</Ele>
-        <CalEle>=</CalEle>
+        <CalEle onClick={() => handleEqual()}>=</CalEle>
       </Row>
     </div>
   );
