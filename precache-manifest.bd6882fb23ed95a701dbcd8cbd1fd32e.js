31dddedb9df7b1a1dc45aeab77704a8b5df6dabe
Cho-Ching Lu <chochinlu@gmail.com>
2019-06-24 10:54:57 +0800

    Updates

---
+++ precache-manifest.bd6882fb23ed95a701dbcd8cbd1fd32e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202261e6c326c7dcd358c45952c9aee1",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "e2f232079b2924cd0875",
+    "url": "/calculator/static/css/main.e97ea7f7.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/calculator/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "e2f232079b2924cd0875",
+    "url": "/calculator/static/js/main.2617b9b8.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/calculator/precache-manifest.b07abbe1984b2b3020d2833e2b618499.js"
+  "/calculator/precache-manifest.bd6882fb23ed95a701dbcd8cbd1fd32e.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/calculator/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
